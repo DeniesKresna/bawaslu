2ffa18a8f3f4554694a7284efafac0c30b86ff42
DeniesKresna <batkorumbawamedsos@gmail.com>
2021-11-13 13:50:25 +0700

    update

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -92,7 +92,7 @@ export function LoginPage({ onGetUser, message, isLoading }) {
                   <img src={logo} style={{maxWidth: "80px"}}/>
                 </Grid>
                 <Grid item xs={10}>
-                  <div style={{color:"white"}}><h1>Sistem aplikasi manajemen aset Bawaslu RIAU - SAMARI</h1></div>
+                  <div><h1>Sistem Aplikasi Manajemen Aset Bawaslu RIAU - SAMARI</h1></div>
                 </Grid>
               </Grid>
             </Grid>
